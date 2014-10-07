c06f6b88fa3888890cc3fd9f09fd0649965ccbf2
John <john@jahewson.com>
2014-10-07 15:30:41 -0700

    Use shared libraries rather than static libraries

--- README.md
+++ README.md
@@ -2,28 +2,12 @@ Tesseract-API
 =============
 This project provides a JNI wrapper for Tesseract OCR Library.
 
-Before building this project 
+Building
+========
 
-Download http://opensource.mxtelecom.com/maven/repo/com/wapmx/native/mx-native-loader/1.2/mx-native-loader-1.2-src.zip
+Dependences: automake, gcc
 
-Extract and run mvn clean install
-
-Building Tesseract API
-=====================
-
-1 - Simple way (This automatically builds all Tesseract and Leptonica static libraries)
-
-Goto porject root directory and run
+cd Tesseract-APIv2
 mvn clean install
 
---------------------------------------------------
-2 - Custom way
-
-Download suitable static libs that are compatible with your system. Ex : Mac OS x64
-liblepta.a and libtesseract.a from sourceforge
-
-Copy them to src/main/resources/libs
-
-Comment out line 48 ( <exec executable="src/main/native/autobuild.sh" failonerror="true">) in pom.xml
-
-Build project using mvn clean install
+This will build all C++ dependencies for the current platform and bundle them into a JAR file.
--- src/main/native/autobuild.sh
+++ src/main/native/autobuild.sh
@@ -1,13 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
 current_dir=$(dirname $0)
-echo "Running autobuild........."
+echo "Running autobuild"
 cd "$current_dir"
-cd src/leptonica/src/src/
-make
-cd ../../../tesseract/src/
-./configure
+
+cd src
+BUILD=$PWD/build
+
+if [ -e $BUILD ]
+then
+  echo "Skipping C++ build as the build directory already exists"
+  exit
+fi
+
+echo "building leptonica"
+cd leptonica-1.71
+./configure --prefix=$BUILD --disable-shared --enable-shared=no --disable-programs --without-zlib --without-libpng --without-jpeg --without-giflib --without-libtiff --without-libwebp --without-libopenjpeg
 make install
-mv tesseract/libs/libtesseract.a tesseract/libs/libtesseracta.a
-cp tesseract/libs/libtesseracta.a ../../../../resources/libs/
-echo "Auto Building all libraries finished"
+cd ..
+
+echo "building tesseract"
+cd tesseract-3.03
+export CPPFLAGS="-I$BUILD/include"
+export LIBLEPT_HEADERSDIR="$BUILD/include"
+./configure --prefix=$BUILD --with-extra-libraries=$BUILD/lib --enable-shared=no --disable-graphics 
+make install
+cd ..
+
+echo "Auto-building all libraries finished"
