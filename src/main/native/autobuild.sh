022edfb40cb2926cc90c430215359374bfcb6933
John <john@jahewson.com>
2014-10-08 08:45:57 -0700

    Allow both OS X and Linux builds

--- src/main/native/autobuild.sh
+++ src/main/native/autobuild.sh
@@ -8,25 +8,39 @@ cd "$current_dir"
 cd src
 BUILD=$PWD/build
 
-if [ -e $BUILD ]
-then
-  echo "Skipping C++ build as the build directory already exists"
-  exit
+# Leptonica refuses to play nice with --enable-shared=no on Linux, trying to statically
+# link libstdc++, so instead we link with -whole-archive in the wrapper Makefile
+if [[ "$(uname)" == 'Linux' ]]; then
+   LEPTONICA_FLAGS='--with-pic'
+   TESSERACT_FLAGS='--with-pic'
+else
+   LEPTONICA_FLAGS='--disable-shared --enable-shared=no'
+   TESSERACT_FLAGS='--enable-shared=no'
 fi
 
-echo "building leptonica"
-cd leptonica-1.71
-mkdir -p m4 # workaround for deprecated autotools functionality on Travis builds
-./configure --prefix=$BUILD --with-pic --disable-programs --without-zlib --without-libpng --without-jpeg --without-giflib --without-libtiff --without-libwebp --without-libopenjpeg
-make install
-cd ..
+if [ -e $BUILD/lib/liblept.a ]
+then
+  echo "Skipping leptonica C++ build as the library already exists"
+else
+  echo "building leptonica"
+  cd leptonica-1.71
+  mkdir -p m4 # workaround for deprecated autotools functionality on Travis builds
+  ./configure --prefix=$BUILD $LEPTONICA_FLAGS --disable-programs --without-zlib --without-libpng --without-jpeg --without-giflib --without-libtiff --without-libwebp --without-libopenjpeg
+  make install
+  cd ..
+fi
 
-echo "building tesseract"
-cd tesseract-3.03
-export CPPFLAGS="-I$BUILD/include"
-export LIBLEPT_HEADERSDIR="$BUILD/include"
-./configure --prefix=$BUILD --with-pic --with-extra-libraries=$BUILD/lib --disable-graphics
-make install
-cd ..
+if [ -e $BUILD/lib/libtesseract.a ]
+then
+  echo "Skipping libtesseract C++ build as the library already exists"
+else
+  echo "building tesseract"
+  cd tesseract-3.03
+  export CPPFLAGS="-I$BUILD/include"
+  export LIBLEPT_HEADERSDIR="$BUILD/include"
+  ./configure --prefix=$BUILD $TESSERACT_FLAGS --with-extra-libraries=$BUILD/lib --disable-graphics
+  make install
+  cd ..
+fi
 
 echo "Auto-building all libraries finished"
